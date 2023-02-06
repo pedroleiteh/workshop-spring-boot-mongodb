2b1c81d35717e2c575b5b8faffb0d7c4b4f7e30c
Pedro Leite <contato.pedro77@gmail.com>
2023-02-06 01:18:34 -0300

    First commit

---
+++ src/main/java/com/snyzen/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.snyzen.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
